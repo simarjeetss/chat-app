8c76ae0d541454af58c134acf2e5749a27b09603
Simarjeet Singh <simarjeetss529@gmail.com>
2023-10-02 21:18:29 +0530

    second commit

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Server {
+import java.util.*;
+import java.io.*;
+import java.net.*;
+public class Server {
+    ServerSocket socket;
+
+    Server(ServerSocket socket){
+        this.socket = socket;
+    }
+
+
+    public void start(){
+        try{
+            while(!socket.isClosed()){
+                System.out.println("Server started, waiting for client to connect!");
+                Socket s = socket.accept();
+            }
+        } catch (IOException e) {
+
+        }
+    }
 }
