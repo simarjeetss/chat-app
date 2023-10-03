76738ddfe05ced7839bbb15e4ec3522fecef3614
Simarjeet Singh <simarjeetss529@gmail.com>
2023-10-03 21:36:09 +0530

    commit

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,8 @@
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,9 @@
-import java.nio.Buffer;
-import java.util.*;
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 public class Server {
     public static void main(String[] args) {
         int port = 10000;
